197722b6dfda31c4900908c61958f57a2f4a7924
blue <1375820104@qq.com>
2021-03-26 19:44:46 +0800

    修正

--- server.js
+++ server.js
@@ -20,23 +20,27 @@ var server = http.createServer(function (request, response) {
     /******** 从这里开始看，上面不要看 ************/
 
     console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
-
-    if (path === '/') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`二哈`)
-        response.end()
-    } else if (path === '/x') {
-        response.statusCode = 200
-        response.setHeader('Content-Type', 'text/css;charset=utf-8')
-        response.write(`body{color: red;}`)
-        response.end()
+    if (path === "/") {
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/html;charset=utf-8");
+        response.write(`
+        <!DOCTYPE html>
+        <link rel="stylesheet" href="/style.css" >
+        <h1>你好</h1>
+        `);
+        response.end();
+    } else if (path === "/style.css") {
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "text/css;charset=utf-8");
+        response.write(`h1{color: red;}`);
+        response.end();
     } else {
-        response.statusCode = 404
-        response.setHeader('Content-Type', 'text/html;charset=utf-8')
-        response.write(`你输入的路径不存在对应的内容`)
-        response.end()
+        response.statusCode = 404;
+        response.setHeader("Content-Type", "text/html;charset=utf-8");
+        response.write(`你访问的页面不存在`);
+        response.end();
     }
+
 
     /******** 代码结束，下面不要看 ************/
 })
